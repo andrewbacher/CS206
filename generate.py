50b0eae37bfad28186f0754b8b6053a1d3e53d4e
andrewbacher <andrew.bacher@uvm.edu>
2021-02-06 16:26:42 -0500

    added a box to our world

--- generate.py
+++ generate.py
@@ -1,5 +1,4 @@
-import pyrosim.pyrosim as pyrosim
-
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box",pos=[0,0,0.5],size=[1,1,1])
-pyrosim.End()
+import pyrosim.pyrosim as pyrosim #we dont need to write pyrosim.pyrosim every time we want to use it
+pyrosim.Start_SDF("box.sdf")#names the world box
+pyrosim.Send_Cube(name="Box",pos=[0,0,0.5],size=[1,1,1])# stores box at specified location
+pyrosim.End()#close sdf file
--- simulate.py
+++ simulate.py
@@ -1,7 +1,10 @@
-import pybullet as p
-import time as t
-physicsClient = p.connect(p.GUI)
-for i in range(1000):
-    p.stepSimulation()
-    t.sleep(0.01666666666)
-p.disconnect()
+import pybullet as p #for creating the simulated environment
+import time as t #t.sleep() function
+physicsClient = p.connect(p.GUI)# object physicsClient handles physics and draws results to a GUI
+
+p.loadSDF("box.sdf")#loads in a box to the environment
+
+for i in range(1000):#loop 1000 times
+    p.stepSimulation()# increase physics inside simulation a small amount
+    t.sleep(0.01666666666)# program waits for 1/60 seconds
+p.disconnect()#quits simulation
