cb5621a27930aa8c827a772541aad2ff62d83080
andrewbacher <andrew.bacher@uvm.edu>
2021-02-11 09:23:27 -0500

    Created the monolith

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import pyrosim.pyrosim as pyrosim #we dont need to write pyrosim.pyrosim every t
 pyrosim.Start_SDF("box.sdf")#names the world box
 #Three variables associated with object size
 length = 1
-width = 1
-height = 1
+width = 2
+height = 3
 pyrosim.Send_Cube(name="Box",pos=[0,0,0.5],size=[length,width,height])# stores box at specified location
 pyrosim.End()#close sdf file
