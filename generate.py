40bbb1489a644882899e4a35d3e2eddfe5f4c812
andrewbacher <andrew.bacher@uvm.edu>
2021-02-11 09:09:10 -0500

    step 13

--- generate.py
+++ generate.py
@@ -1,4 +1,8 @@
 import pyrosim.pyrosim as pyrosim #we dont need to write pyrosim.pyrosim every time we want to use it
 pyrosim.Start_SDF("box.sdf")#names the world box
-pyrosim.Send_Cube(name="Box",pos=[0,0,0.5],size=[1,1,1])# stores box at specified location
+#Three variables associated with object size
+length = 1
+width = 1
+height = 1
+pyrosim.Send_Cube(name="Box",pos=[0,0,0.5],size=[length,width,height])# stores box at specified location
 pyrosim.End()#close sdf file
