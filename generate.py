774828d29541b18086f00d9cfc2793af04ace1af
andrewbacher <andrew.bacher@uvm.edu>
2021-02-14 16:28:40 -0500

    simulate minimal world

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 import pyrosim.pyrosim as pyrosim #we dont need to write pyrosim.pyrosim every time we want to use it
-pyrosim.Start_SDF("boxes.sdf")#names the world box
+pyrosim.Start_SDF("world.sdf")#names the world box
 #Three variables associated with object size
 length = 1
 width = 1
@@ -7,21 +7,7 @@ height = 1
 #three variables for object position
 x = 0
 y = 0
-z = 0.35
-for k in range(4):
-    y = y +1
-    for j in range(4):
-        x = x+1
-        for i in range(9):
-            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])  # stores box at specified location
-            z = z*.9
-            z = z+1
-            length = length*.9
-            width = width*.9
-            height = height*.9
-        height = 1
-        length = 1
-        width = 1
-        z = .35
-    x = 0
+z = 0.5
+pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])  # stores box at specified location
+
 pyrosim.End()#close sdf file
