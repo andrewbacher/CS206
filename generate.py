ef6332fcc2c18896853631e2426d73899e3392f9
andrewbacher <andrew.bacher@uvm.edu>
2021-02-14 17:25:19 -0500

    Finished Assignment 2

--- generate.py
+++ generate.py
@@ -15,9 +15,11 @@ def Create_World():
     pyrosim.End()  # close sdf file
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")#Unified Robot Description Format file stores description of robot body
-    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])  # stores the cube "Torso" at specified location
-    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position="0.5 0 1.0")
-    pyrosim.Send_Cube(name="Leg", pos=[0.5, 0, 0.5], size=[1, 1, 1])  # stores the cube "Leg" at specified location
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])  # stores the cube "Torso" at specified location
+    pyrosim.Send_Joint(name="Torso_Bleg", parent="Torso", child="Bleg", type="revolute", position="1 0 1")
+    pyrosim.Send_Cube(name="Bleg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])  # stores the cube "Bleg" at specified location
+    pyrosim.Send_Joint(name="Torso_Fleg", parent="Torso", child="Fleg", type="revolute", position="2 0 1")
+    pyrosim.Send_Cube(name="Fleg", pos=[0.5, 0, -0.5], size=[1, 1, 1])  # stores the cube "Fleg" at specified location
 
     pyrosim.End()
 Create_World()
