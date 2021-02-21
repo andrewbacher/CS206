61e0fb11916622341233631c0fa1179feccbb1f0
andrewbacher <andrew.bacher@uvm.edu>
2021-02-21 18:05:22 -0500

    plotted data from sensors

--- analyze.py
+++ analyze.py
@@ -6,8 +6,7 @@ frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 
 print(backLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues)
-matplotlib.pyplot.show()
-matplotlib.pyplot.plot(frontLegSensorValues)
-
+matplotlib.pyplot.plot(backLegSensorValues, label = "Bleg",linewidth = 3)
+matplotlib.pyplot.plot(frontLegSensorValues, label = "Fleg")
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
