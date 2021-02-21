9c52640b94ef0621f0be55c48aee6c6e80b6c680
andrewbacher <andrew.bacher@uvm.edu>
2021-02-21 15:31:45 -0500

    stored sensor values in array

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p #for creating the simulated environment
 import time as t #t.sleep() function
 import pybullet_data
 import pyrosim.pyrosim as pyrosim #we dont need to write pyrosim.pyrosim every time we want to use it
+import numpy
 
 
 
@@ -13,8 +14,13 @@ planeId = p.loadURDF("plane.urdf")
 planeId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")#loads in a box to the environment
 pyrosim.Prepare_To_Simulate("body.urdf")
-for i in range(1000):#loop 1000 times
+backLegSensorValues = numpy.zeros(100)
+
+for i in range(100):#loop 1000 times
     p.stepSimulation()# increase physics inside simulation a small amount
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("Bleg")
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Bleg")
+
     t.sleep(0.01666666666)# program waits for 1/60 seconds
 p.disconnect()#quits simulation
+
+print(backLegSensorValues)
