f3803e3556ffdf8ae9cfff940c7c0986589908a7
andrewbacher <andrew.bacher@uvm.edu>
2021-02-26 16:57:00 -0500

    added motor to back leg torso joint

--- simulate.py
+++ simulate.py
@@ -11,9 +11,12 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)#simulate gravity
 
 planeId = p.loadURDF("plane.urdf")
-planeId = p.loadURDF("body.urdf")
+robot = p.loadURDF("body.urdf")
+
 p.loadSDF("world.sdf")#loads in a box to the environment
+
 pyrosim.Prepare_To_Simulate("body.urdf")
+
 backLegSensorValues = numpy.zeros(100)
 frontLegSensorValues = numpy.zeros(100)
 
@@ -21,6 +24,19 @@ for i in range(100):#loop 1000 times
     p.stepSimulation()# increase physics inside simulation a small amount
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Bleg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Fleg")
+
+    pyrosim.Set_Motor_For_Joint(
+
+        bodyIndex=robot,
+
+        jointName="Torso_Bleg",
+
+        controlMode=p.POSITION_CONTROL,
+
+        targetPosition=0.0,
+
+        maxForce=500)
+
     t.sleep(0.01666666666)# program waits for 1/60 seconds
 p.disconnect()#quits simulation
 numpy.save("data/backLegSensorValues.npy",backLegSensorValues)#saves sensor values in a file
