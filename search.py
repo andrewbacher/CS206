d82616a99de94a2d86c107328aee0cd2dd84c54f
andrewbacher <andrew.bacher@uvm.edu>
2021-03-27 16:56:58 -0400

    Random 3x2 matrix of synaptic weights

--- search.py
+++ search.py
@@ -1,6 +1,8 @@
 import os
+from hillclimber import HILL_CLIMBER
+from solution import SOLUTION
 
-
-for i in range(5):
-    os.system("py generate.py")
-    os.system("py simulate.py")
+hc = HILL_CLIMBER()
+# for i in range(5):
+#     os.system("py generate.py")
+#     os.system("py simulate.py")
---
+++ solution.py
@@ -0,0 +1,7 @@
+import numpy
+
+class SOLUTION:
+    def __init__(self):
+        self.solution = {}
+        self.weights = numpy.random.rand(3,2)
+        self.weights * 2 - 1
