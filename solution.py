39ce94b97498601b63dff39fb41bd8d7fd279a1e
andrewbacher <andrew.bacher@uvm.edu>
2021-04-10 12:54:47 -0400

    Step 47 fully formed quadruped

--- solution.py
+++ solution.py
@@ -65,6 +65,18 @@ class SOLUTION:
 
         pyrosim.Send_Joint(name="Torso_Rleg", parent="Torso", child="Rleg", type="revolute", position="0.5 0 1",jointAxis="0 1 0")
         pyrosim.Send_Cube(name="Rleg", pos=[0.5, 0, 0], size=[1.0, 0.2, 0.2])
+
+        pyrosim.Send_Joint(name="Fleg_FrontLowerLeg", parent="Fleg", child="FrontLowerLeg", type="revolute", position="0 1 0",jointAxis="1 0 0")
+        pyrosim.Send_Cube(name="FrontLowerLeg", pos=[0, 0, -0.5], size=[0.2, 0.2, 1])
+
+        pyrosim.Send_Joint(name="Bleg_BackLowerLeg", parent="Bleg", child="BackLowerLeg", type="revolute",position="0 -1 0", jointAxis="1 0 0")
+        pyrosim.Send_Cube(name="BackLowerLeg", pos=[0, 0, -0.5], size=[0.2, 0.2, 1])
+
+        pyrosim.Send_Joint(name="Lleg_LeftLowerLeg", parent="Lleg", child="LeftLowerLeg", type="revolute",position="-1 0 0", jointAxis="0 1 0")
+        pyrosim.Send_Cube(name="LeftLowerLeg", pos=[0, 0, -0.5], size=[0.2, 0.2, 1])
+
+        pyrosim.Send_Joint(name="Rleg_RightLowerLeg", parent="Rleg", child="RightLowerLeg", type="revolute",position="1 0 0", jointAxis="0 1 0")
+        pyrosim.Send_Cube(name="RightLowerLeg", pos=[0, 0, -0.5], size=[0.2, 0.2, 1])
         pyrosim.End()
 
 
@@ -75,10 +87,20 @@ class SOLUTION:
         pyrosim.Send_Sensor_Neuron(name=2, linkName="Fleg")
         pyrosim.Send_Sensor_Neuron(name=3, linkName="Lleg")
         pyrosim.Send_Sensor_Neuron(name=4, linkName="Rleg")
-        pyrosim.Send_Motor_Neuron(name=5, jointName="Torso_Bleg")
-        pyrosim.Send_Motor_Neuron(name=6, jointName="Torso_Fleg")
-        pyrosim.Send_Motor_Neuron(name=7, jointName="Torso_Lleg")
-        pyrosim.Send_Motor_Neuron(name=8, jointName="Torso_Rleg")
+        pyrosim.Send_Sensor_Neuron(name=5, linkName="BackLowerLeg")
+        pyrosim.Send_Sensor_Neuron(name=6, linkName="FrontLowerLeg")
+        pyrosim.Send_Sensor_Neuron(name=7, linkName="LeftLowerLeg")
+        pyrosim.Send_Sensor_Neuron(name=8, linkName="RightLowerLeg")
+
+
+        pyrosim.Send_Motor_Neuron(name=8, jointName="Torso_Bleg")
+        pyrosim.Send_Motor_Neuron(name=10, jointName="Torso_Fleg")
+        pyrosim.Send_Motor_Neuron(name=11, jointName="Torso_Lleg")
+        pyrosim.Send_Motor_Neuron(name=12, jointName="Torso_Rleg")
+        pyrosim.Send_Motor_Neuron(name=13, jointName="Bleg_BackLowerLeg")
+        pyrosim.Send_Motor_Neuron(name=14, jointName="Fleg_FrontLowerLeg")
+        pyrosim.Send_Motor_Neuron(name=15, jointName="Lleg_LeftLowerLeg")
+        pyrosim.Send_Motor_Neuron(name=16, jointName="Rleg_RightLowerLeg")
         # pyrosim.Send_Synapse(sourceNeuronName= 0, targetNeuronName= 3, weight= 1.0)
         # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1.0)
 
@@ -89,8 +111,10 @@ class SOLUTION:
         # pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=1.0)
         for currentRow in range(c.numSensorNeurons):
             for currentColumn in range(c.numMotorNeurons):
-                pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn+5, weight=self.weights[currentRow][currentColumn])
+                pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn+9, weight=self.weights[currentRow][currentColumn])
         pyrosim.End()
+
+
 
 
     def Mutate(self):
